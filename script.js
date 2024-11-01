5ddf1d1b22b4496f3c305a29fc7ea536f0bc7c0e
Ellyda Micaella <ellydamicaella810@gmail.com>
2024-10-31 23:38:13 -0300

    subindo alterações

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('form-contato').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Mensagem enviada! Em breve entrarei em contato.');
+    this.reset();
+});
